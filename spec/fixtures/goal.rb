87ddcb7a3e59838d746f554a7cef47959c9b3dcc
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:21:41 +0000

    Automatically backed up by Learn

--- spec/fixtures/goal.rb
+++ spec/fixtures/goal.rb
@@ -14,7 +14,7 @@
     :gender => ["male"],
     :lives => ["Central Park"]
   },
-  "Ms .K" => {
+  "Ms K." => {
     :color => ["grey", "white"],
     :gender => ["female"],
     :lives => ["Central Park"]
@@ -35,3 +35,27 @@
     :lives => ["Central Park"]
   }
 }
+
+def pigeon_data = {
+   :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+}
+end 
+
+def nyc_pigeon_organizer(data)
+ 
+end

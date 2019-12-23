ee9bddf752f95155619e67d2f7c1910eb0ed5666
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:32:37 +0000

    Automatically backed up by Learn

--- spec/fixtures/goal.rb
+++ spec/fixtures/goal.rb
@@ -36,26 +36,6 @@ def nyc_pigeon_data = {
   }
 }
 
-def pigeon_data = {
-   :color => {
-          :purple => ["Theo", "Peter Jr.", "Lucky"],
-          :grey => ["Theo", "Peter Jr.", "Ms. K"],
-          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-          :brown => ["Queenie", "Alex"]
-        },
-        :gender => {
-          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-          :female => ["Queenie", "Ms. K"]
-        },
-        :lives => {
-          "Subway" => ["Theo", "Queenie"],
-          "Central Park" => ["Alex", "Ms. K", "Lucky"],
-          "Library" => ["Peter Jr."],
-          "City Hall" => ["Andrew"]
-        }
-}
-end 
-
 def nyc_pigeon_organizer(pigeon_data)
  pigeon_list = {}
     data.each do |color_gender_lives, value|

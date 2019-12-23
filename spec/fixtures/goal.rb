177625ec634123a52ee39bf125a1f01735f819a6
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:37:55 +0000

    Automatically backed up by Learn

--- spec/fixtures/goal.rb
+++ spec/fixtures/goal.rb
@@ -37,17 +37,5 @@ def pigeon_list = {
 }
 
 def nyc_pigeon_organizer(data)
- pigeon_list = {}
-
-  data.each do |attribute_name, attributes|
-    attributes.each do |attribute_value, pigeon_names|
-      pigeon_names.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][attribute_name] ||= []
-        pigeon_list[name][attribute_name].push(attribute_value.to_s)
-      end
-    end 
-  end
-
-  pigeon_list
-end
+ 
+ end 

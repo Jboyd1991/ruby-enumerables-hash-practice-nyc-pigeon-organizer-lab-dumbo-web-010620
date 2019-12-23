1ffdeb4f86b84f15f5c0c78cad57b8bc00eaf063
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:34:13 +0000

    Automatically backed up by Learn

--- spec/fixtures/goal.rb
+++ spec/fixtures/goal.rb
@@ -38,18 +38,16 @@
 
 def nyc_pigeon_organizer(pigeon_data)
  pigeon_list = {}
-    data.each do |color_gender_lives, value|
-    value.each do |stats, all_names|
-    all_names.each do |name|
-if pigeon_list[name] == nil
-    pigeon_list[name] = {}
+
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_name] ||= []
+        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+      end
+    end 
   end
-if pigeon_list[name][color_gender_lives] == nil
-    pigeon_list[name][color_gender_lives] = []
-  end
-    pigeon_list[name][color_gender_lives].push(stats.to_s)
-  end
-  end 
-  end
-    pigeon_list
+
+  pigeon_list
 end
